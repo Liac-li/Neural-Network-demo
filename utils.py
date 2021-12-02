8116ba47e039e300ef7dd685b39cc62f988da0f1
leelin <lyt-42@outlook.com>
2021-12-03 07:37:15 +0800

    Add util.py as helper functions collection

---
+++ utils.py
@@ -0,0 +1,29 @@
+# Helper functions
+import os
+import gzip
+import numpy as np
+
+
+def load_minst_data(data_folder):
+    files = [
+        'train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz',
+        't10k-labels-idx1-ubyte.gz', 't10k-images-idx3-ubyte.gz'
+    ]
+    print(f'[Log] load local MNIST Data from {data_folder}')
+
+    path = []
+    for fname in files:
+        path.append(os.path.join(data_folder, fname))
+
+    with gzip.open(path[0], 'rb') as lbpath:
+        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+    with gzip.open(path[1], 'rb') as imgpath:
+        x_train = np.frombuffer(imgpath.read(), np.uint8,
+                                offset=16).reshape(len(y_train), 28, 28)
+    with gzip.open(path[2], 'rb') as lbpath:
+        y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+    with gzip.open(path[3], 'rb') as imgpath:
+        x_test = np.frombuffer(imgpath.read(), np.uint8,
+                               offset=16).reshape(len(y_test), 28, 28)
+
+    return (x_train, y_train), (x_test, y_test)
